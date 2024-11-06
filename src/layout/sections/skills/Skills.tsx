5c185d9d088327c2ceb897d59a0c6a03629c1157
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-07 00:51:05 +0300

    add skills

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {Icon} from "../../components/icon/Icon";
+import {SectionTitle} from "../../components/SectionTitle";
+import {Skill} from "./skill/Skill";
+
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+            <SectionTitle>My Skills</SectionTitle>
+            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                <Skill iconId={"htmlSvg"} title={"html5"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"cssSvg"} title={"css"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"jsSvg"} title={"js"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"reactSvg"} title={"react"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"gitSvg"} title={"git"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"githubSvg"} title={"github"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+            </FlexWrapper>
+        </StyledSkills>
+    );
+};
+
+
+const StyledSkills = styled.section`
+    background-color: #ffd1f2;
+    min-height: 100vh;
+`
