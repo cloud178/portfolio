7ae03328f6d57e64cf4eb8cc007613961fb14553
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-06 22:44:49 +0300

    add Header

---
+++ src/layout/components/logo/Logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+import styled from "styled-components";
+
+export const Logo = () => {
+    return (
+        <Link href="#">
+            <Icon iconId={"logoSvg"} width={"60"} height={"60"} viewBox={"0 0 64 64"}/>
+        </Link>
+    );
+};
+
+
+const Link = styled.a`
+    display: inline-flex;
+`
