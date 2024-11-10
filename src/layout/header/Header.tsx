9678697610c08f39fcc5812d73deff071a976d03
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-10 23:39:05 +0300

    add HeaderMenu to Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../components/logo/Logo";
-import {Menu} from "../components/menu/Menu";
+import {Container} from "../components/Container";
+import {FlexWrapper} from "../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 
 const menuItems = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact']
@@ -9,17 +11,23 @@ const menuItems = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact']
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={menuItems}/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <HeaderMenu menuItems={menuItems}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 
 const StyledHeader = styled.header`
-    background-color: #a6fff3;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0;
+    background-color: rgba(31, 31, 32, 0.9);
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 900;
 `
---
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+export const HeaderMenu = (props: {menuItems: Array<string>}) => {
+    return (
+        <StyledHeaderMenu>
+            <ul>
+                {props.menuItems.map((item, index) => {
+                    return <ListItem key={index}>
+                        <Link href="#">
+                            {item}
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                        </Link>
+                    </ListItem>
+                })}
+            </ul>
+        </StyledHeaderMenu>
+    );
+};
+
+
+const StyledHeaderMenu = styled.nav`
+    ul {
+        display: flex;
+        gap: 35px;
+        justify-content: center;
+    }
+`
+
+const Link = styled.a`
+    font-family: Josefin Sans, sans-serif;
+    font-size: 25px;
+    font-weight: 400;
+    text-align: center;
+    color: transparent;
+`
+
+const Mask = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    height: 50%;
+    overflow: hidden;
+    //outline: 1px solid red;
+    color: ${theme.colors.font.fontHeader};
+    
+    & + & {
+        top: 50%;
+        span {
+            display: inline-block;
+            transform: translateY(-50%);
+        }
+    }
+`
+
+const ListItem = styled.li`
+    position: relative;
+    
+    &::before {
+        content: "";
+        display: inline-block;
+        height: 2px;
+        background-color: ${theme.colors.font.fontHeader};
+        
+        position: absolute;
+        top: 50%;
+        left: -10px;
+        right: -10px;
+        z-index: 100;
+        
+        transform: scale(0);
+    }
+
+    &:hover {
+        transform: scale(1.2);
+        
+        &::before {
+            transform: scale(1);
+        }
+        
+        ${Mask} {
+            transform: skewX(-15deg) translateX(5px);
+            color: ${theme.colors.font.fontPrimary};
+
+            & + ${Mask} {
+                transform: skewX(-15deg) translateX(-5px);
+            }
+        }
+
+        
+    }
+`
