82d2a14a6bd9872e2cbe89dc4bf8807e35c075c8
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-10 23:34:16 +0300

    add Theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        primary: '#1A1A1A',
+        secondary: '#262626',
+
+        font: {
+            fontPrimary: '#FFFFFF',
+            fontHeader: '#2DCB87',
+        }
+    }
+}
