7ae2ebca9be9452fabdb9c51411bd1b8816b71e6
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-13 21:59:03 +0300

    add padding 100px 0 for all of the sections

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -34,6 +34,10 @@ export const GlobalStyle = createGlobalStyle `
         border: none;
     }
     
+    section {
+        padding: 100px 0;
+    }
+    
     section:nth-of-type(even) {
         background-color: ${theme.colors.primaryBg};
     }
