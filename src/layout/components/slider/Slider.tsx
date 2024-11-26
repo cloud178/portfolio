65564bff768c3d411542b395743a42282b1db1b1
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-26 18:14:59 +0300

    continue doing slider

--- src/layout/components/slider/Slider.tsx
+++ src/layout/components/slider/Slider.tsx
@@ -1,47 +1,59 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import {S} from './Slider_Styles';
-import {Link} from "../Link";
-import webTemplate from "../../../assets/images/blog-web-template.webp"
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { S } from "./Slider_Styles";
+import { Link } from "../Link";
+import webTemplate from "../../../assets/images/blog-web-template.webp";
+import "../../../styles/slider.css"
 
 type SlidePropsType = {
-    title: string,
-    // text: string,
-    // img: string,
-}
+    title: string;
+    text: string;
+    img: any;
+};
 
 const Slide = (props: SlidePropsType) => {
     return (
         <S.Slide>
-            <S.Image src={webTemplate}/>
+            <S.Image src={props.img} />
             <div>
                 <S.Title>{props.title}</S.Title>
-                <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua Ut enim
-                </S.Text>
+                <S.Text>{props.text}</S.Text>
                 <Link>Learn more</Link>
             </div>
         </S.Slide>
-    )
-}
+    );
+};
 
 const items = [
-    <Slide title={"How to make web templates"}/>,
-    <Slide title={"How1 to make web templates"}/>,
-    <Slide title={"How2 to make web templates"}/>,
+    <Slide
+        img={webTemplate}
+        title={"How to make web templates"}
+        text={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        }
+    />,
+    <Slide
+        img={webTemplate}
+        title={"1 How to make web templates"}
+        text={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        }
+    />,
+    <Slide
+        img={webTemplate}
+        title={"2 How to make web templates"}
+        text={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        }
+    />,
 ];
 
 export const Slider = () => (
     <S.Slider>
-        <AliceCarousel
-            mouseTracking
-            items={items}
-        />
+        <AliceCarousel mouseTracking items={items} />
     </S.Slider>
-
 );
-
 
 // import React from 'react';
 // import webTemplate from "../../../assets/images/blog-web-template.webp"
