d3cf0026214bbdf777b55e5a3f49ffa277fd4225
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-30 22:05:10 +0300

    add emailjs

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, {ElementRef, useRef} from 'react'
 import {SectionTitle} from '../../components/SectionTitle'
 import {Button} from '../../components/Button'
 import {Container} from "../../components/Container";
 import {S} from './Contacts_Styles';
 import {Fade} from "react-awesome-reveal";
+import emailjs from '@emailjs/browser';
 
 export const Contacts = () => {
+
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+
+        if (!form.current) return;
+
+        emailjs
+            .sendForm('service_ilw7yeg', 'template_f6faums', form.current, {
+                publicKey: 'xbjnqg6ol40BFKOVj',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.target.reset() // когда письмо отправится, эта строка когда очистит форму, тем самым юзер пойсёт что письмо отправлено
+    };
+
+
     return (
         <S.Contacts id='contact'>
             <Container>
                 <SectionTitle>Contacts</SectionTitle>
                 <Fade delay={200}>
-                    <S.Form>
-                        <S.Field placeholder='John Doe'/>
-                        <S.Field placeholder='example@mail.com'/>
-                        <S.Field as={"textarea"} placeholder='Message'/>
-                        <Button type={"submit"}>Send message</Button>
-                    </S.Form>
+                    <> {/* заглушка иначе fade конфликтует с emailjs и письмо не отправляется */}
+                        <S.Form ref={form} onSubmit={sendEmail}>
+                            <S.Field placeholder='*name' name={'user_name'} required/>
+                            <S.Field placeholder='*email' name={'user_email'} type={"email"} required/>
+                            <S.Field placeholder='*subject' name={'subject'} required/>
+                            <S.Field as={"textarea"} placeholder='*Message' name={'message'} required/>
+                            <Button type={"submit"}>Send message</Button>
+                        </S.Form>
+                    </>
                 </Fade>
             </Container>
         </S.Contacts>
